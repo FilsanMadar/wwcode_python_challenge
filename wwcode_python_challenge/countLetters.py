a942b811c76b1f5fdc460b553ece4b5477d94310
FilsanMadar <filsanmadar@gmail.com>
2024-01-17 23:13:12 -0500

    day 8 - refactored code to not have to set alphabet and used built in string methods instead. added docstring to explain the code.

--- wwcode_python_challenge/countLetters.py
+++ wwcode_python_challenge/countLetters.py
@@ -15,23 +15,23 @@ numVowels()
 
 # Day 8 : Write a function that accepts a string and calculates the number of uppercase and lowercase letters in it.
 
-def typeLetters():
+def typeLetters(input_string):
+    """
+        Counts and returns the number of uppercase and lowercase letters in the input string.
 
-    input_string = str(input("Enter your sentence here to calculate the uppercase and lowercase letters are present: "))
-    upper_letters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    lower_letters = set("abcdefghijklmnopqrstuvwxyz")
-    count1 = 0
-    count2 = 0
+        Parameters:
+        input_string (str): The string to be analyzed.
 
-    for char in input_string:
-        if char in upper_letters:
-            count1 = count1 + 1
-        if char in lower_letters:
-            count2 = count2 + 1
+        Returns:
+        tuple: A tuple containing the counts of uppercase and lowercase letters.
+    """
+    upper_count = sum(1 for char in input_string if char.isupper())
+    lower_count = sum(1 for char in input_string if char.islower())
+    return lower_count, upper_count
 
-    print("The number of uppercase letters is: " + str(count1))
-    print("The number of lowercase letters is: " + str(count2))
+input_string = str(input("Enter your sentence here to calculate the uppercase and lowercase letters are present: "))
+upper_count, lower_count = typeLetters(input_string)
 
-typeLetters()
-
+print("The number of uppercase letters is: ", upper_count)
+print("The number of lowercase letters is: ", lower_count)
 
