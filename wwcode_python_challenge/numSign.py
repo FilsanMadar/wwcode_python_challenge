76f516899fff1d4e16c77148e854b5ea2046ab7c
FilsanMadar <filsanmadar@gmail.com>
2024-01-18 18:37:00 -0500

    day 9 : check to see if the number is even or odd

--- wwcode_python_challenge/numSign.py
+++ wwcode_python_challenge/numSign.py
@@ -10,3 +10,16 @@ def returnSign(num):
 
 num = float(input("Enter a number: "))
 print(returnSign(num))
+
+# Day 9 : Write a program to check if a number is even or odd
+
+def even_and_odd():
+
+    inputNum = int(input("Enter your numerical value: "))
+
+    if (inputNum % 2) == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+even_and_odd()
