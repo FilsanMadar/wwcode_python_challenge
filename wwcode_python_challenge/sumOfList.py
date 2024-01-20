8a4c94532783052ac9d98fcfdbc72928626a31c0
FilsanMadar <filsanmadar@gmail.com>
2024-01-20 00:12:32 -0500

    Day 10

--- wwcode_python_challenge/sumOfList.py
+++ wwcode_python_challenge/sumOfList.py
@@ -10,5 +10,17 @@ def sumOfList():
     sumofNum = sum(numList)
 # convert your list back to a string when your ready to print
     print("The sum of your numbers are: "+ str(sumofNum))
-
 sumOfList()
+
+# Day 10 : Write a program to remove duplicates from a list.
+
+def remove_duplicate():
+    given_num_list = input("Enter a list of numbers: ")
+    # convert input string into list
+    # use set() to remove duplicate from list
+    remove_list = list(set(given_num_list))
+    print("The list after removing the duplicates is: " + str(remove_list))
+
+remove_duplicate()
+
+
