ee0da61122bb7d70a3ded228082688b088e0edc0
FilsanMadar <filsanmadar@gmail.com>
2024-01-22 19:27:40 -0500

    Day 12 : reverse a given string

--- wwcode_python_challenge/strOccurances.py
+++ wwcode_python_challenge/strOccurances.py
@@ -1,7 +1,6 @@
 # Day 6 : Write a program to count the occurrences of a specific character in a string.
 
 def strOccurances():
-
     inputString = input("Enter a sentence: ")
     target_char = input("Which character do you want to count? ")
     count = 0
@@ -12,6 +11,22 @@ def strOccurances():
 
     print(f"The number of character occurances of {target_char} in {inputString} is: {count}")
 
+
 strOccurances()
 
 
+# Day 12 : Write a program to reverse a given string.
+
+def reverse_string(input_str):
+    return input_str[::-1]
+
+
+# Take user input
+original_str = input("Enter a string: ")
+
+# Reverse the string
+reversed_str = reverse_string(original_str)
+
+# print
+print("Original String:", original_str)
+print("Reversed String:", reversed_str)
