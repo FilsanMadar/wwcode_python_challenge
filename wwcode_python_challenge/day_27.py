6d6536cd1a888bf9daa0d7e6e54f58163dee3293
FilsanMadar <filsanmadar@gmail.com>
2024-02-05 18:20:25 -0500

    Day 27 and renamed files to just day numbers

---
+++ wwcode_python_challenge/day_27.py
@@ -0,0 +1,11 @@
+# Day 27: Create a program that sorts a list of strings alphabetically.
+
+def sort_alphabetically():
+
+    given_input = input("Enter your input here: ").split()
+
+    sorted_input = sorted(given_input)
+
+    print(sorted_input)
+
+sort_alphabetically()
--- wwcode_python_challenge/day_4_10_13_20.py
+++ wwcode_python_challenge/day_4_10_13_20.py
@@ -55,21 +55,5 @@ def list_only_even_numbers():
 
 print("List of only even numbers: ", list_only_even_numbers())
 
-# Day 22 : Create a program to find the second-largest element in a list.
 
-def second_max_list():
-# get a list of numbers
-    given_numbers = input("Enter list of numbers: ").split("")
-
-    given_numbers = [int(num) for num in given_numbers]
-# determine the max value of a given list
-    max_value = max(given_numbers)
-# remove the max value from the list
-    given_numbers.remove(max_value)
-# determine the new max value
-    max_value2 = max(given_numbers)
-
-    print("The second largest element is: ", max_value2)
-
-second_max_list()
 
